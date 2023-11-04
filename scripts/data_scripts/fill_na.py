e89a113fe6710d0394e6e22fb227867e1bd2a2ec
Angry <Sannikovayi@gmail.com>
2023-11-04 14:58:06 +0300

    change dvc.yaml

--- scripts/data_scripts/fill_na.py
+++ scripts/data_scripts/fill_na.py
@@ -24,18 +24,20 @@ def process_data(fd_in, fd_out):
         arr_education_num.append(line[0])
         arr_capital_gain.append(line[1])
         arr_sex.append(line[2])
-        if line[3]:
+        if line[3].isdigit():
             arr_age.append(float(line[3]))
         else:
             arr_age.append(0)
         arr_education.append(line[4])
         arr_marital_status.append(line[5])
 
-    s = sum(arr_age)
+    age_sum = sum(arr_age)
+    age_count = len([age for age in arr_age if age > 0])
+    average_age = age_sum / age_count if age_count > 0 else 0
 
     for i in range(len(arr_age)):
         if arr_age[i] == 0:
-            arr_age[i] = round(s / len(arr_age), 2)
+            arr_age[i] = round(average_age, 2)
 
     for p_education_num, p_capital_gain, p_sex, p_age, p_education, p_marital_status in zip(arr_education_num, arr_capital_gain, arr_sex, arr_age, arr_education, arr_marital_status):
         fd_out.write("{},{},{},{},{},{}\n".format(p_education_num, p_capital_gain, p_sex, p_age, p_education, p_marital_status))
